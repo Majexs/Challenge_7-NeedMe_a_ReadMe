1ec1f8e1ff53ea2ffac0a0f097789688823b68d7
Majexs <166327239+Majexs@users.noreply.github.com>
2024-09-02 11:04:36 -0400

    added npm dependencies and questions

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 import { renderLicenseBadge, renderLicenseLink, renderLicenseSection } from './utils/generateMarkdown';
 import generateMarkdown from './utils/generateMarkdown';
+import badge-maker from 'badge-maker';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the Title of your Project?',
+    'Provide a short Description of your Project',
+    'Would you like to add a Table of Contents?',
+    'Please provide instructions and examples for Usage',
+    'Are there any other collaborators you would like to Credit?',
+    'Would you like to add a License?',
+    'Add a Badge?',
+    'Are there any Features you would like to include?',
+    'How can others Contribute?',
+    'Are there any example Tests you woud like to have the user run?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    for (i = 0, i < questions.length, i++) {
+        .prompt ([{
+            type = 'input',
+            message = `${questions[i]}`,
+        }])
+    }
 }
 
 // TODO: Create a function to initialize app
